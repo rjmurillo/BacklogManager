059975fc47d218e4eafb97666326dd58b6bc00f2
Richard Murillo <Rjmurillo@msn.com>
2015-03-16 03:31:47 -0700

    Addition of convention suppressions and config defaults
    Removed the cascade delete default. In addtion, added blanket configuration for string values such that they should be limited to some value instead of being NVARCHAR(MAX)

--- src/BacklogManager/DAL/BacklogDbContext.cs
+++ src/BacklogManager/DAL/BacklogDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
 using BacklogManager.Models;
 
 namespace BacklogManager.DAL
@@ -8,7 +9,8 @@ namespace BacklogManager.DAL
     {
         public BacklogDbContext()
             : base("BacklogDbContext")
-        { }
+        {
+        }
         public DbSet<BacklogItem> BacklogItems { get; set; }
 
         public DbSet<User> Users { get; set; }
@@ -17,6 +19,20 @@ namespace BacklogManager.DAL
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+            // Otherwise everything is NVARCHAR(MAX)
+            modelBuilder.Properties<string>().Configure(c => c.HasMaxLength(4000));
+        }
+    }
+
+    public static class BacklogDbContextExtensions
+    {
+        public static IQueryable<BacklogItem> CompleteBacklogItems(this BacklogDbContext context)
+        {
+            return context.BacklogItems
+                .Include(p => p.Owner)
+                .Include(p => p.Project);
         }
     }
 }
