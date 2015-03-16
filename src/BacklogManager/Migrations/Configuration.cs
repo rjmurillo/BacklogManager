60e835fb7b4c576e18ba5c913e3156729cd6a355
Richard Murillo <Rjmurillo@msn.com>
2015-03-15 21:43:30 -0700

    Remove extra stories. Now one per user
    Easier to catch data dupe bugs now

--- src/BacklogManager/Migrations/Configuration.cs
+++ src/BacklogManager/Migrations/Configuration.cs
@@ -41,22 +41,7 @@ namespace BacklogManager.Migrations
                 new BacklogItem {ID=2, Owner = users[1], Upvotes = 3, Discipline = "manager", Action = "auto create tasks from deliverables for my ICs", Goal = "I have more time to code"},
                 new BacklogItem {ID=3, Owner = users[2], Upvotes = 4, Discipline = "program manager", Action = "see a daily report on bugs in my area path", Goal = "I can calculate my triage rate"},
                 new BacklogItem {ID=4, Owner = users[3], Upvotes = 8, Discipline = "developer", Action = "see a breakdown of my time spent on tasks", Goal = "I can identify which tasks takes me longer"},
-                new BacklogItem {ID=5, Owner = users[4], Upvotes = 0, Discipline = "developer", Action = "count how many reduction bugs I can delegate", Goal = "I spend more time building features"},
-                new BacklogItem {ID=6, Owner = users[0], Upvotes = 15, Discipline = "software engineer", Action = "adopt an effective process", Goal = "I can be more productive"},
-                new BacklogItem {ID=7, Owner = users[1], Upvotes = 3, Discipline = "manager", Action = "auto create tasks from deliverables for my ICs", Goal = "I have more time to code"},
-                new BacklogItem {ID=8, Owner = users[2], Upvotes = 4, Discipline = "program manager", Action = "see a daily report on bugs in my area path", Goal = "I can calculate my triage rate"},
-                new BacklogItem {ID=9, Owner = users[3], Upvotes = 8, Discipline = "developer", Action = "see a breakdown of my time spent on tasks", Goal = "I can identify which tasks takes me longer"},
-                new BacklogItem {ID=10, Owner = users[4], Upvotes = 0, Discipline = "developer", Action = "count how many reduction bugs I can delegate", Goal = "I spend more time building features"},
-                new BacklogItem {ID=11, Owner = users[0], Upvotes = 15, Discipline = "software engineer", Action = "adopt an effective process", Goal = "I can be more productive"},
-                new BacklogItem {ID=12, Owner = users[1], Upvotes = 3, Discipline = "manager", Action = "auto create tasks from deliverables for my ICs", Goal = "I have more time to code"},
-                new BacklogItem {ID=13, Owner = users[2], Upvotes = 4, Discipline = "program manager", Action = "see a daily report on bugs in my area path", Goal = "I can calculate my triage rate"},
-                new BacklogItem {ID=14, Owner = users[3], Upvotes = 8, Discipline = "developer", Action = "see a breakdown of my time spent on tasks", Goal = "I can identify which tasks takes me longer"},
-                new BacklogItem {ID=15, Owner = users[4],  Upvotes = 0, Discipline = "developer", Action = "count how many reduction bugs I can delegate", Goal = "I spend more time building features"},
-                new BacklogItem {ID=16, Owner = users[0], Upvotes = 15, Discipline = "software engineer", Action = "adopt an effective process", Goal = "I can be more productive"},
-                new BacklogItem {ID=17, Owner = users[1], Upvotes = 3, Discipline = "manager", Action = "auto create tasks from deliverables for my ICs", Goal = "I have more time to code"},
-                new BacklogItem {ID=18, Owner = users[2], Upvotes = 4, Discipline = "program manager", Action = "see a daily report on bugs in my area path", Goal = "I can calculate my triage rate"},
-                new BacklogItem {ID=19, Owner = users[3], Upvotes = 8, Discipline = "developer", Action = "see a breakdown of my time spent on tasks", Goal = "I can identify which tasks takes me longer"},
-                new BacklogItem {ID=20, Owner = users[4], Upvotes = 0, Discipline = "developer", Action = "count how many reduction bugs I can delegate", Goal = "I spend more time building features"}
+                new BacklogItem {ID=5, Owner = users[4], Upvotes = 0, Discipline = "developer", Action = "count how many reduction bugs I can delegate", Goal = "I spend more time building features"}
             };
 
 
